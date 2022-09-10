38e06947a145b6f308fdd01d30cbcf86cec5f5c3
Handzic Haris <handzicharis62@yahoo.com>
2022-09-11 01:48:09 +0200

    package installs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: [
+                    '.ios.js',
+                    '.android.js',
+                    '.js',
+                    '.ts',
+                    '.tsx',
+                    '.json'
+                ],
+                alias: {
+                    '@tests': ['./tests/'],
+                    '@components': './src/components',
+                    '@assets': './src/assets',
+                    '@theme': './src/theme',
+                    '@constants': './src/constants',
+                    '@helpers': './src/helpers',
+                    '@languages': './src/languages',
+                    '@hooks': './src/hooks',
+                    '@navigation': './src/navigation',
+                    '@redux': './src/redux',
+                    '@screens': './src/screens',
+                    '@sockets': './src/sockets',
+                    '@types': './src/types'
+                }
+            }
+        ]
+    }
+}
